30f5d26b48c2574276736b1a01bb9b158e1588a8
Javier Navarrete <rgonzalez@mutualidadabogacia.com>
2023-02-23 17:10:50 +0100

    Anado estilos

--- src/app/components/filter-input/filter-input.component.ts
+++ src/app/components/filter-input/filter-input.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-filter-input',
   templateUrl: './filter-input.component.html',
+  styleUrls: ['./filter-input.component.css']
 })
 export class FilterInputComponent implements OnInit {
 
